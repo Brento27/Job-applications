1b2773b0b1b6e9a114f93d09cc9e2c401c6b5334
Brent Lombaard <99014529+Brento27@users.noreply.github.com>
2022-10-25 18:17:10 +0200

    Finished employees and department templates

--- techGenius/frontend/src/screens/DepartmentList.jsx
+++ techGenius/frontend/src/screens/DepartmentList.jsx
@@ -11,7 +11,63 @@ function DepartmentList() {
         </div>
         <div className='w-full h-full'>
           <p className='font-bold text-3xl'>Departments</p>
-          <div className='border-2 border-fuchsia-700 mt-2 w-10/12 h-64'></div>
+          <div className='border-2 border-fuchsia-700 mt-5 w-10/12 flex flex-col p-4 gap-4'>
+            <p className='absolute top-40 left-45 bg-default px-2'>filters</p>
+            <div className='flex gap-6 items-center justify-between'>
+              <p>Status</p>
+              <select className='select select-accent w-full max-w-xs '>
+                <option selected>Active Only</option>
+                <option>(All)</option>
+                <option>Deactive Only</option>
+              </select>
+            </div>
+            <button className='btn btn-accent w-24'>Filter</button>
+          </div>
+          <div className='flex justify-between mt-4'>
+            <div className='flex gap-6 items-center'>
+              <p>Show per Page</p>
+              <select className='select select-accent'>
+                <option selected disabled>
+                  10 / 20 / 50 / 100 / all
+                </option>
+                <option>(All)</option>
+                <option>Deactive Only</option>
+              </select>
+            </div>
+            <div className='flex items-center'>
+              <input
+                type='text'
+                placeholder='Search'
+                className='input input-bordered input-accent'
+              />
+            </div>
+          </div>
+          <div className='overflow-x-auto mt-4'>
+            <table className='table w-full'>
+              <thead>
+                <tr>
+                  <th>Actions</th>
+                  <th>Name</th>
+                  <th>Manager</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Edit Deactive</th>
+                  <th>Test Dept</th>
+                  <td>TestSurname</td>
+                  <td>Active</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className='btn-group mb-16'>
+            <button className='btn btn-xs'>1</button>
+            <button className='btn btn-xs btn-active'>2</button>
+            <button className='btn btn-xs'>3</button>
+            <button className='btn btn-xs'>4</button>
+          </div>
         </div>
       </div>
     </div>
--- techGenius/frontend/src/screens/EmployeeList.jsx
+++ techGenius/frontend/src/screens/EmployeeList.jsx
@@ -9,9 +9,94 @@ function EmployeeList() {
         <div className='border-2 border-fuchsia-700 w-1/6 h-96'>
           <p className='text-center'>Menu</p>
         </div>
-        <div className='w-full h-full'>
+        <div className='w-full'>
           <p className='font-bold text-3xl'>Employees</p>
-          <div className='border-2 border-fuchsia-700 mt-2 w-10/12 h-64'></div>
+          <div className='border-2 border-fuchsia-700 mt-5 w-10/12 h-72 flex flex-col p-4 gap-4'>
+            <p className='absolute top-40 left-45 bg-default px-2'>filters</p>
+            <div className='flex gap-6 items-center justify-between'>
+              <p>Status</p>
+              <select className='select select-accent w-full max-w-xs '>
+                <option selected disabled>
+                  Active Only / (All) / Deactive Only
+                </option>
+                <option>Active Only</option>
+                <option>(All)</option>
+                <option>Deactive Only</option>
+              </select>
+            </div>
+            <div className='flex gap-6 items-center justify-between'>
+              <p>Department</p>
+              <select className='select select-accent w-full max-w-xs justify-self-end'>
+                <option selected disabled>
+                  -select-
+                </option>
+                <option>(All)</option>
+                <option>Deactive Only</option>
+              </select>
+            </div>
+            <div className='flex gap-6 items-center justify-between'>
+              <p>Manager</p>
+              <select className='select select-accent w-full max-w-xs'>
+                <option selected disabled>
+                  -select-
+                </option>
+                <option>(All)</option>
+                <option>Deactive Only</option>
+              </select>
+            </div>
+            <button className='btn btn-accent w-24'>Filter</button>
+          </div>
+          <div className='flex justify-between mt-4'>
+            <div className='flex gap-6 items-center'>
+              <p>Show per Page</p>
+              <select className='select select-accent'>
+                <option selected disabled>
+                  10 / 20 / 50 / 100 / all
+                </option>
+                <option>(All)</option>
+                <option>Deactive Only</option>
+              </select>
+            </div>
+            <div className='flex items-center'>
+              <input
+                type='text'
+                placeholder='Search'
+                className='input input-bordered input-accent'
+              />
+            </div>
+          </div>
+          <div className='overflow-x-auto mt-4'>
+            <table className='table w-full'>
+              <thead>
+                <tr>
+                  <th>Actions</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Telephone Number</th>
+                  <th>Email Address</th>
+                  <th>Manager</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Edit Deactive</th>
+                  <th>Test Name</th>
+                  <td>TestSurname</td>
+                  <td>0821675514</td>
+                  <td>test@test.com</td>
+                  <td></td>
+                  <td>Active</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className='btn-group mb-16'>
+            <button className='btn btn-xs'>1</button>
+            <button className='btn btn-xs btn-active'>2</button>
+            <button className='btn btn-xs'>3</button>
+            <button className='btn btn-xs'>4</button>
+          </div>
         </div>
       </div>
     </div>
